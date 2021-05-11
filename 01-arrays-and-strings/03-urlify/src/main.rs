034ed09e3b170bb674f19f01d86ca497d122721d
Bernardo Meurer <bernardo@meurer.org>
2021-05-10 20:12:28 -0700

    01-arrays-and-strings/03-urlify: improve urlify-in-place

--- 01-arrays-and-strings/03-urlify/src/main.rs
+++ 01-arrays-and-strings/03-urlify/src/main.rs
@@ -6,18 +6,19 @@ fn urlify(s: &str) -> String {
 
 fn urlify_in_place(v: &mut Vec<char>) {
     // first we must resize the vec to make sure we can fit the replacements
-    let original_len = v.len();
-    let space_count = v.iter().filter(|c| c.is_whitespace()).count();
+    let trailing_whitespace_count = v.iter().rev().take_while(|c| c.is_whitespace()).count();
+    let string_text_len = v.len() - trailing_whitespace_count;
+    let space_count = v[0..string_text_len]
+        .iter()
+        .filter(|c| c.is_whitespace())
+        .count();
     let extra_space_needed = space_count * (URL_WHITESPACE.len() - 1);
-    let needed_len = original_len + extra_space_needed;
-    if original_len < needed_len {
-        v.resize(needed_len, ' ');
-    }
+    let total_space_needed = string_text_len + extra_space_needed;
+    v.resize(total_space_needed, ' ');
 
     // now we replace " " with %20
-
     let mut idx = v.len();
-    for i in (0..original_len).rev() {
+    for i in (0..string_text_len).rev() {
         if v[i].is_whitespace() {
             v[idx - 1] = '0';
             v[idx - 2] = '2';
@@ -31,26 +32,38 @@ fn urlify_in_place(v: &mut Vec<char>) {
 }
 
 fn main() {
-    print!("checking urlify: ");
-    let a = "Mr John Smith     ";
+    let a = "Mr John Smith      ";
+    let a_url = "Mr%20John%20Smith";
     let b = "                  ";
+    let b_url = "";
     let c = "I am a tab ->	";
+    let c_url = "I%20am%20a%20tab%20->";
     let d = "tab ->	<- tab";
-    assert_eq!(urlify(a), "Mr%20John%20Smith");
-    assert_eq!(urlify(b), "");
-    assert_eq!(urlify(c), "I%20am%20a%20tab%20->");
-    assert_eq!(urlify(d), "tab%20->%20<-%20tab");
+    let d_url = "tab%20->%20<-%20tab";
+
+    print!("checking urlify: ");
+    assert_eq!(urlify(a), a_url);
+    assert_eq!(urlify(b), b_url);
+    assert_eq!(urlify(c), c_url);
+    assert_eq!(urlify(d), d_url);
     println!("OK");
 
     print!("checking urlify_in_place: ");
-    let mut a = "Mr John Smith".chars().collect::<Vec<_>>();
-    let mut b = "".chars().collect::<Vec<_>>();
-    let mut d = "tab ->	<- tab".chars().collect::<Vec<_>>();
+    let mut a = a.chars().collect::<Vec<_>>();
+    let a_url = a_url.chars().collect::<Vec<_>>();
+    let mut b = b.chars().collect::<Vec<_>>();
+    let b_url = b_url.chars().collect::<Vec<_>>();
+    let mut c = c.chars().collect::<Vec<_>>();
+    let c_url = c_url.chars().collect::<Vec<_>>();
+    let mut d = d.chars().collect::<Vec<_>>();
+    let d_url = d_url.chars().collect::<Vec<_>>();
     urlify_in_place(&mut a);
     urlify_in_place(&mut b);
+    urlify_in_place(&mut c);
     urlify_in_place(&mut d);
-    assert_eq!(a, "Mr%20John%20Smith".chars().collect::<Vec<_>>());
-    assert_eq!(b, "".chars().collect::<Vec<_>>());
-    assert_eq!(d, "tab%20->%20<-%20tab".chars().collect::<Vec<_>>());
+    assert_eq!(a, a_url);
+    assert_eq!(b, b_url);
+    assert_eq!(c, c_url);
+    assert_eq!(d, d_url);
     println!("OK");
 }
