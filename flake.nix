6a59dc161edc4ccb12cb241970377aad25a317e4
Bernardo Meurer <bernardo@meurer.org>
2021-05-10 17:41:21 -0700

    treewide: init

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    fenix = {
+      url = "github:figsoda/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, fenix, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        fenixPkgs = fenix.packages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # Rust
+            (fenixPkgs.complete.withComponents [
+              "cargo"
+              "clippy-preview"
+              "rust-src"
+              "rust-std"
+              "rustc"
+              "rustfmt-preview"
+            ])
+            fenixPkgs.rust-analyzer
+            cargo-edit
+          ];
+        };
+      });
+}
